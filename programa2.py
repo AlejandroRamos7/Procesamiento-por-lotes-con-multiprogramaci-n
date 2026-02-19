e15f128b048a7f359ddd410b8b7f4845cd59a449
Yobanny Alejandro Ramos Plata <Alejandroplata711@gmail.com>
2026-02-18 23:24:46 -0600

    programa2-funcionando

--- programa2.py
+++ programa2.py
@@ -9,7 +9,10 @@ def limpiar():
 def generar_datos(num):
     procesos = []
     lista_id = []
+
+
     for i in range(num):
+
         while True:
             id_programador = random.randint(0,1000)
             if id_programador in lista_id:
@@ -17,20 +20,15 @@ def generar_datos(num):
             else:             
                 lista_id.append(id_programador)             
                 break    
-        
-        while True:
-            tiempo_maximo = random.randint(6, 20)
-            if tiempo_maximo < 6 or tiempo_maximo > 20:   # CORREGIDO
-                print("Tiempo no valido")         
-            else:                        
-                break
+
+        tiempo_maximo = random.randint(6, 20)
 
         operaciones = ["+", "-", "*", "/", "%", "**"]
         operacion = random.choice(operaciones)
-        
+
         a = random.randint(1,10)
         b = random.randint(1,10) 
-        
+
         if operacion == "+":
             resultado =  a+b
         elif operacion == "-":
@@ -43,9 +41,11 @@ def generar_datos(num):
             resultado =  a%b
         elif operacion == "**":
             resultado =  a**b
-        
+
         procesos.append([id_programador, tiempo_maximo, operacion, a, b, resultado, 0])     
+
     return procesos
+
 
 def crear_lotes(procesos):
     lotes = []     
@@ -53,26 +53,33 @@ def crear_lotes(procesos):
         lotes.append(procesos[i:i+5])     
     return lotes  
 
-def mostrar(lotes):     
+
+def mostrar(lotes):
+
     procesados = []     
     tiempo = 0     
-    numero_de_lotes = 0         
+    numero_de_lotes = 0       
 
-    while numero_de_lotes < len(lotes):         
-        lote = lotes[numero_de_lotes]         
-        
+    while numero_de_lotes < len(lotes):
+        lote = lotes[numero_de_lotes]   
+
         print("----------------------------------------------")
         print(f"LOTE EN EJECUCION: {numero_de_lotes + 1}")
         for p in lote:
             print(f"ID: {p[0]} | Tiempo Maximo: {p[1]}")
         print("----------------------------------------------")
+        n= 0
+        while n < len(lote):
+            i = lote[n]
+            if i in procesados:
+                n+=1
+                continue
 
-        for i in lote:             
             tiempo_estimado = i[6]
-            interrumpido = False                     
+            interrumpido = False 
 
-            while tiempo_estimado < i[1]:            
-                
+            while tiempo_estimado < i[1]:
+
                 if msvcrt.kbhit():
                     tecla = msvcrt.getch().decode().upper()
 
@@ -82,15 +89,17 @@ def mostrar(lotes):
                             if msvcrt.kbhit():
                                 if msvcrt.getch().decode().upper() == "C":
                                     break
+
                     elif tecla == "I":
                         i[6] = tiempo_estimado
-                        lote.append(lote.pop(lote.index(i)))
+                        lote.append(lote.pop(n))
                         interrumpido = True
                         break
 
                     elif tecla == "E":
                         i[5] = "ERROR"
                         break
+
                 limpiar()
 
                 pendientes = len(lotes) - numero_de_lotes - 1  
@@ -99,7 +108,7 @@ def mostrar(lotes):
                 print(f"Lotes pendientes: {pendientes}")                 
                 print(f"Lote actual: {numero_de_lotes + 1}")                 
                 print("------------------------------------------------")     
-                
+
                 print("------------------------------------------------")                 
                 print(f"PROCESOS DEL LOTE: {numero_de_lotes+1}")                 
                 for ejecucion in lote:
@@ -115,26 +124,30 @@ def mostrar(lotes):
                     f"Tiempo restante: {i[1]-tiempo_estimado}"                 
                 ]                  
 
-                terminados = []         
+                terminados = []   
 
-                for j in procesados:                     
+                for j in procesados:
+
                     if isinstance(j[5], str):
                         resultado = j[5]
                     else:
                         resultado = f"{j[5]:.2f}"
-                    
+
                     separador = "-------------------------------------------------------"
 
                     terminados.extend([
-                        separador,                         
+                        separador,
+                        f"LOTE: {j[7]}",
                         f"ID: {j[0]} / Operacion: {j[2]} / Valores: {j[3]} y {j[4]} / Resultado: {resultado}",
                         separador
-                    ])                  
+                    ])
 
                 from itertools import zip_longest                 
                 ancho = 45                 
+
                 print(f"{'PROCESO ACTUAL':^{ancho}}|{'PROCESOS TERMINADOS':^{ancho}}")                 
                 print("------------------------------------------------")                 
+
                 for izq, der in zip_longest(actual, terminados, fillvalue=""):                     
                     print(f"{str(izq):<{ancho}}|{str(der):<{ancho}}")                                  
 
@@ -143,17 +156,25 @@ def mostrar(lotes):
                 print()                   
 
                 time.sleep(1.5)                 
+
                 tiempo += 1                 
                 tiempo_estimado += 1             
+                i[6] = tiempo_estimado
 
-            if not interrumpido:
-                procesados.append(i)                 
+            if interrumpido:
+                continue
+            
+            i.append(numero_de_lotes + 1)
+            procesados.append(i)
+            n+=1                 
 
         numero_de_lotes += 1     
 
     print("-------------------------------")     
     print("PROCESOS COMPLETOS FINALES")     
-    for i in procesados:         
+
+    for i in procesados:
+
         print("----------------------------------------------------------")                  
         print (f"ID: {i[0]}")         
 
@@ -163,6 +184,7 @@ def mostrar(lotes):
             print (f"Operacion: {i[2]} Valores: {i[3]} y {i[4]} Resultado: {float(i[5]):.2f}")
 
     input("\nPresione ENTER para finalizar...")  
+
 
 num = int(input("Ingrese el numero de procesos que desea ingresar: "))
 
